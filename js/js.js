9cf113eaebc1644c1a57d615e8d15ccdb93d84ce
Pomawo <Romeoihnatenko@gmail.com>
2023-03-13 11:48:25 +0200

    commti

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+    $('.serachmenu').hide()
+    $('.div_search').hide()
+    $('.menu').click(()=>{
+        $('.serachmenu').show(800)
+         $('.div_search').show(1000)
+    })
+
+})
