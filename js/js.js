51fd23ca249e53d4ecfe9df153f934dec40e6d39
talafarael <127351345+talafarael@users.noreply.github.com>
2023-03-13 14:29:03 +0200

    Delete js.js

--- js/js.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(()=>{
-    $('.serachmenu').hide()
-    $('.div_search').hide()
-    $('.menu').click(()=>{
-        $('.serachmenu').show(800)
-         $('.div_search').show(1000)
-    })
-
-})
